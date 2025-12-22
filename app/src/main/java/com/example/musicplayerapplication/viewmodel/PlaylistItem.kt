be7824c4761479ec1ee46bb4734cf6035934bab1
pradeepta3456 <pratimahub323@gmail.com>
2025-12-22 18:45:16 +0545

    made settings and playlist

---
+++ app/src/main/java/com/example/musicplayerapplication/viewmodel/PlaylistItem.kt
@@ -0,0 +1,16 @@
+package com.example.musicplayerapplication.viewmodel
+
+import androidx.compose.ui.graphics.Brush
+import com.example.musicplayerapplication.Song
+
+data class Playlist(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val icon: androidx.compose.ui.graphics.vector.ImageVector,
+    val gradient: Brush,
+    val songs: MutableList<Song>
+)
+
+
+
---
+++ app/src/main/java/com/example/musicplayerapplication/viewmodel/navitemmodel.kt
@@ -0,0 +1,6 @@
+package com.example.musicplayerapplication.viewmodel
+
+data class navItem(
+    val label: String,
+    val icon: Int
+)
