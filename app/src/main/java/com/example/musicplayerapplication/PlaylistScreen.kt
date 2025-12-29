2531610b7d56fae7dc219e5af0fe0758df8552dd
pradeepta3456 <pratimahub323@gmail.com>
2025-12-29 21:09:42 +0545

    Added playlist setting screen

--- app/src/main/java/com/example/musicplayerapplication/PlaylistScreen.kt
+++ app/src/main/java/com/example/musicplayerapplication/PlaylistScreen.kt
@@ -3,6 +3,7 @@ package com.example.musicplayerapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -45,10 +46,8 @@ data class Playlist(
     val songs: MutableList<Song>
 )
 
-
-
 @Composable
-fun Playlist() {
+fun PlaylistScreen() {
     // All songs database
     val allSongs = remember {
         mutableStateListOf(
